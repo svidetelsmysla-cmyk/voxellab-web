5e680a90cb2b7a6ae0a1cf490311b4550077b8bf
svidetelsmysla-cmyk <svidetelsmysla@gmail.com>
2026-08-05 21:28:40 +0300

    Install A0.2 display semantic repair

--- src/actionLab/main.ts
+++ src/actionLab/main.ts
@@ -1,6 +1,9 @@
 import "./styles.css";
 import "./stylesA02.css";
 import { ActionTransportAppA02 } from "./AppA02";
+import { installA02DisplaySemanticsRepair } from "./a02DisplaySemanticsRepair";
+
+installA02DisplaySemanticsRepair();
 
 const root = document.querySelector<HTMLElement>("#action-app");
 if (!root) throw new Error("Missing #action-app root");
